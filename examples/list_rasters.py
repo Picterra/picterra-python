c9940984a90a459356d0b7d28f9e0a98090a4e52
Julien Rebetez <julien.rebetez@picterra.ch>
2020-02-12 11:45:37 +0100

    Rename APIClient functions to be more conversational

--- examples/list_rasters.py
+++ examples/list_rasters.py
@@ -1,5 +1,5 @@
 from picterra import APIClient
 
 client = APIClient(api_key='1234')
-for raster in client.rasters_list():
+for raster in client.list_rasters():
     print('raster id=%s, name=%s, status=%s' % (raster['id'], raster['name'], raster['status']))
