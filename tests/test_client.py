69804b0b32a61c00ea0ceedc0e93213992a32f64
Julien Rebetez <julien.rebetez@picterra.ch>
2020-02-12 11:29:46 +0100

    Add rasters_set_detection_areas_from_file method

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,4 @@
+import tempfile
 import responses
 from picterra import APIClient
 from urllib.parse import urljoin
@@ -30,6 +31,48 @@ def add_mock_rasters_list_response():
     responses.add(responses.GET, api_url('rasters/'), json=data, status=200)
 
 
+def add_mock_detection_areas_upload_responses(raster_id):
+    upload_id = 42
+    # Upload initiation
+    data = {
+        'upload_url': 'http://storage.example.com',
+        'upload_id': upload_id
+    }
+    responses.add(
+        responses.POST,
+        api_url('rasters/%s/detection_areas/upload/file/' % raster_id), json=data, status=200)
+
+    # Storage PUT
+    responses.add(responses.PUT, 'http://storage.example.com', status=200)
+
+    # Commit
+    data = {
+        'poll_interval': 0.1,
+    }
+    responses.add(
+        responses.POST,
+        api_url('rasters/%s/detection_areas/upload/%s/commit/' % (raster_id, upload_id)),
+        json=data, status=200)
+
+    # Status, first check
+    data = {
+        'status': 'processing'
+    }
+    responses.add(
+        responses.GET,
+        api_url('rasters/%s/detection_areas/upload/%s/' % (raster_id, upload_id)),
+        json=data, status=200)
+
+    # Status, second check
+    data = {
+        'status': 'ready'
+    }
+    responses.add(
+        responses.GET,
+        api_url('rasters/%s/detection_areas/upload/%s/' % (raster_id, upload_id)),
+        json=data, status=200)
+
+
 @responses.activate
 def test_rasters_list():
     client = _client()
@@ -37,3 +80,14 @@ def test_rasters_list():
     rasters = client.rasters_list()
     assert rasters[0]['name'] == 'raster1'
     assert rasters[1]['name'] == 'raster2'
+
+
+@responses.activate
+def test_rasters_set_detection_areas_from_file():
+    add_mock_detection_areas_upload_responses(1)
+
+    client = _client()
+    # This just tests that this doesn't raise
+    with tempfile.NamedTemporaryFile() as f:
+        client.raster_set_detection_areas_from_file(1, f.name)
+
