3847ec26d468fedd3a9120da11c4bd1f4dccf91d
Julien Rebetez <julien@fhtagn.net>
2020-06-05 14:25:46 +0200

    Set version and remove unnecessary options from setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ from setuptools import setup
 
 setup(
     name='picterra',
-    version='0',
+    version='1.0.0',
     description='Picterra API client',
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    include_package_data=True,
-    zip_safe=False,
     setup_requires=[
         'pytest-runner',
         'flake8',
