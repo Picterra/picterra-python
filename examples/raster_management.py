906fd83061d99c33e23fae80a9be54c491183616
Andrea Orlandi <andrea.orlandi@picterra.ch>
2020-10-05 16:32:07 +0200

    Unify raster examples

--- examples/raster_management.py
+++ examples/raster_management.py
@@ -1,5 +1,12 @@
 from picterra import APIClient
 
 client = APIClient(api_key='1234')
+
+raster_id = client.upload_raster('data/raster1.tif', name='a nice raster')
+print('Uploaded raster=', raster_id)
+
 for raster in client.list_rasters():
     print('raster id=%s, name=%s, status=%s' % (raster['id'], raster['name'], raster['status']))
+
+client.delete_raster(raster_id)
+print('Deleted raster=', raster_id)
--- examples/upload_raster.py
+++
@@ -1,5 +0,0 @@
-from picterra import APIClient
-
-client = APIClient(api_key='1234')
-raster_id = client.upload_raster('data/raster1.tif', name='a nice raster')
-print('Uploaded raster=', raster_id)
