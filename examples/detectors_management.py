7ae5e7ac0b95b07af08aa3c2c0209a2df2eb7cf9
Andrea Orlandi <andrea.orlandi@picterra.ch>
2020-10-22 16:01:18 +0200

    Add edit_detector
    Closes #36.

--- examples/detectors_management.py
+++ examples/detectors_management.py
@@ -7,8 +7,13 @@ client = APIClient()
 # Create a new detector (its type is 'count' by default)
 detector_id = client.create_detector('My first detector')
 
+# Edit the above detector
+client.edit_detector(detector_id, 'Renamed detector', 'segmentation', 'bbox', 1000)
 
 # List existing detectors
 for d in client.list_detectors():
     print('detector id=%s, name=%s, detection_type=%s, output_type=%s, training_steps=%d' % (
-        d['id'], d['name'], d['detection_type'], d['output_type'], d['training_steps']))
+        d['id'], d['name'],
+        d['configuration']['detection_type'], d['configuration']['output_type'],
+        d['configuration']['training_steps']
+    ))
