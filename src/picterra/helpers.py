4fbe3d854a5bf5439be98c3042d32fcb884fb452
Andrea Orlandi <andrea.orlandi@picterra.ch>
2022-08-25 08:12:05 +0200

    Remove unused helpers, add one for argparse range validation

--- src/picterra/helpers.py
+++ src/picterra/helpers.py
@@ -1,23 +1,32 @@
-from argparse import ArgumentParser, Namespace
-from typing import Dict
-import sys
-from logging import Logger
-from .client import APIClient, APIError
+from argparse import ArgumentParser, ArgumentTypeError
+from typing import Union
 
 class InvalidOptionError(Exception):
-    """TODO"""
+    """Exception carrying a parser"""
     def __init__(self, parser: ArgumentParser):            
         super().__init__('Invalid option error')
         self.parser = parser
 
-ParserMap = Dict[str, ArgumentParser]
 
-class CommandParser():
-    """TODO"""
-    def __init__(self, name: str, parser: ArgumentParser, logger: Logger):  
-        """TODO"""          
-        self.name = name
-        self.parser = parser
-        self.logger = logger
-        self.parsers_map = None
-        
+def range_limited_type(min: Union[int, float], max: Union[int, float]):
+    """
+    Return function handle of an argument type function for
+       ArgumentParser checking a float/int range: mini <= arg <= maxi
+         mini - minimum acceptable argument
+         maxi - maximum acceptable argument
+    """
+
+    # Define the function with default arguments
+    def range_limited_type_checker(arg):
+        """ Type function for argparse - a number within some predefined bounds """
+        typ = type(min)
+        try:
+            v = typ(arg)
+        except ValueError:
+            raise ArgumentTypeError("Must be " + str(typ))
+        if v < min or v > max:
+            raise ArgumentTypeError("Argument must be < " + str(min) + "and > " + str(max))
+        return arg
+
+    # Return function handle to checking function
+    return range_limited_type_checker
