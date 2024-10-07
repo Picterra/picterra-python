e0c7c30a05d313f6914dbddbaa1d657f404aca13
Stephane Restani <stephane.restani@picterra.ch>
2024-10-07 09:25:39 +0200

    Bump package version to 2.0.2

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 # read the contents of your README file
 import sys
 from pathlib import Path
+
 from setuptools import find_packages, setup
 
 this_directory = Path(__file__).parent
@@ -16,7 +17,7 @@ test_deps = ["pytest==7.1", "responses==0.22", "httpretty"]
 
 setup(
     name="picterra",
-    version="2.0.1",
+    version="2.0.2",
     description="Picterra API client",
     long_description=long_description,
     long_description_content_type="text/markdown",
