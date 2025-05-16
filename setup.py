40f7c020cf5febf479d78516c566369dcda2cb89
Colin Silcock <colin.silcock@picterra.ch>
2025-05-16 11:25:49 +0100

    Update the plots analysis api client to support data ingestion pipeline

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ test_deps = ["pytest==7.1", "responses==0.22", "httpretty"]
 
 setup(
     name="picterra",
-    version="2.0.5",
+    version="2.0.6",
     description="Picterra API client",
     long_description=long_description,
     long_description_content_type="text/markdown",
