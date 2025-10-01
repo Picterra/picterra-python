daee2b4c12b5e052cee6f2c5497a44976758b23a
Julien Rebetez <julien.rebetez@picterra.ch>
2025-10-01 10:04:54 +0200

    [refactor] Use new simplified urls for Tracer public API
    This adapts the TracerClient to use the new simplified urls which don't
    require repeating e.g. plots_group_id when accessing an analysis.
    
    Those arguments are still accepted by the various python functions but
    ignored and they will raise a DeprecationWarning

--- tests/test_tracer_client.py
+++ tests/test_tracer_client.py
@@ -295,10 +295,10 @@ def test_download_plots_group(monkeypatch):
 @responses.activate
 def test_list_plots_analysis_reports(monkeypatch):
     client: TracerClient = _client(monkeypatch, platform="plots_analysis")
-    url = plots_analysis_api_url("plots_groups/my-pg-id/analysis/my-analysis-id/reports/")
+    url = plots_analysis_api_url("plots_analyses/my-analysis-id/reports/")
     # Full list
     add_mock_paginated_list_response(url, num_results=3)
-    reports = client.list_plots_analysis_reports("my-analysis-id", "my-pg-id")
+    reports = client.list_plots_analysis_reports("my-analysis-id")
     assert len(reports) == 3
     assert reports[0]["name"] == "a_1" and reports[-1]["name"] == "a_3"
 
@@ -306,7 +306,7 @@ def test_list_plots_analysis_reports(monkeypatch):
 @responses.activate
 def test_list_plots_analysis_report_types(monkeypatch):
     client: TracerClient = _client(monkeypatch, platform="plots_analysis")
-    url = plots_analysis_api_url("plots_groups/my-pg-id/analysis/my-analysis-id/reports/types/")
+    url = plots_analysis_api_url("plots_analyses/my-analysis-id/reports/types/")
     responses.get(
         url,
         json=[
@@ -316,7 +316,7 @@ def test_list_plots_analysis_report_types(monkeypatch):
             {"report_type": "type_3", "name": "a_4"},
         ],
     )
-    reports = client.list_plots_analysis_report_types("my-analysis-id", "my-pg-id")
+    reports = client.list_plots_analysis_report_types("my-analysis-id")
     assert len(reports) == 4
     assert reports[0]["report_type"] == "type_1" and reports[-1]["name"] == "a_4"
 
@@ -335,7 +335,7 @@ def test_create_plots_analysis_report_precheck(monkeypatch):
         "plot_ids": ["uno", "dos"],
     })])
     _add_api_response(plots_analysis_api_url(
-        "plots_groups/a-group-id/analysis/an-analysis-id/reports/precheck/"),
+        "plots_analyses/an-analysis-id/reports/precheck/"),
         responses.POST,
         OP_RESP,
         match=responses.matchers.json_params_matcher({
@@ -358,7 +358,6 @@ def test_create_plots_analysis_report_precheck(monkeypatch):
         "foobar",
         ["uno", "dos"],
         "a-report-type",
-        "a-group-id",
         metadata={"foo": "bar"}
     ) == {"status": "passed"}
 
@@ -377,7 +376,7 @@ def test_create_plots_analysis_report(monkeypatch):
         "plot_ids": ["uno", "dos"],
     })])
     _add_api_response(plots_analysis_api_url(
-        "plots_groups/a-group-id/analysis/an-analysis-id/reports/"),
+        "plots_analyses/an-analysis-id/reports/"),
         responses.POST,
         OP_RESP,
         match=responses.matchers.json_params_matcher({
@@ -400,7 +399,6 @@ def test_create_plots_analysis_report(monkeypatch):
         "foobar",
         ["uno", "dos"],
         "a-report-type",
-        "a-group-id",
         metadata={"foo": "bar"}
     ) == "a-report-id"
 
@@ -409,7 +407,7 @@ def test_create_plots_analysis_report(monkeypatch):
 def test_get_plots_analysis(monkeypatch):
     client: TracerClient = _client(monkeypatch, platform="plots_analysis")
     _add_api_response(
-        plots_analysis_api_url("plots_groups/a-plots-group/analysis/an-analysis-id/"),
+        plots_analysis_api_url("plots_analyses/an-analysis-id/"),
         responses.GET,
         {
             "id": "an-analysis-id",
@@ -419,7 +417,7 @@ def test_get_plots_analysis(monkeypatch):
             "url": "https://app.picterra.ch/plots_analysis/plots_groups/136b812e-8d9c-418f-b317-8be5c7c6281d/analysis/cda443d7-5baf-483d-bb5e-fa1190180b0d/"  # noqa[E501]
         },
     )
-    plots_analysis = client.get_plots_analysis("an-analysis-id", "a-plots-group")
+    plots_analysis = client.get_plots_analysis("an-analysis-id")
     assert plots_analysis["id"] == "an-analysis-id"
     assert plots_analysis["name"] == "My Analysis"
 
@@ -428,7 +426,7 @@ def test_get_plots_analysis(monkeypatch):
 def test_get_plots_analysis_report(monkeypatch):
     _add_api_response(
         plots_analysis_api_url(
-            "plots_groups/a-group-id/analysis/a-analysis-id/reports/a-report-id/"
+            "plots_analysis_reports/a-report-id/"
         ),
         responses.GET,
         {
@@ -457,6 +455,6 @@ def test_get_plots_analysis_report(monkeypatch):
         },
     )
     client: TracerClient = _client(monkeypatch, platform="plots_analysis")
-    report = client.get_plots_analysis_report("a-report-id", "a-group-id", "a-analysis-id")
+    report = client.get_plots_analysis_report("a-report-id")
     assert report["id"] == "a-report-id"
     assert report["artifacts"][0]["name"] == "EUDR Report"
